72f43d69b1770d794cfacfb51a683ee3eba3fec0
IPv21 <willsherman771@gmail.com>
2025-02-23 20:12:31 -0500

    starting project

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,17 @@
-import { use, useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './index.css'
+import React from "react";
+import "./index.css";
 
-function Card({ title, rating }) {
-  const [hasLiked, setHasLiked] = useState(false);
-  const [count, setCount] = useState(0);
-  useEffect(() => { 
-    console.log(`The card ${title} has been liked: ${hasLiked}`);
-  }
-  , [hasLiked]);
-
-
-
-  return (
-    <div className="card" onClick={() => setCount(count + 1)}>
-      <h2>{title} 42:45<br/>{ count || null }{rating}</h2>
-      <button onClick={() => setHasLiked(!hasLiked)}>
-        {hasLiked ? '❤️' : 'Like'}
-      </button>
-    </div>
-  )
-}
 
 function App() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">SaY HELLO to THESE!~</h1> {
 
-
-      return (
-        <div className="card-container">
-          <Card title="Star Wars" isCool={true} />
-          <Card title="Avatar"/>
-          <Card title="Kinds"/>
-
-
-</div>
-      )
+          
+    
+    }
+    </div>
+  );
 }
 
-
-
-export default App
+export default App;
